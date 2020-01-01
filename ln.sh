7894a24a796aec7a0f37f48777b78d7329068273
jarra <suhrawardi@gmail.com>
2020-01-01 21:04:20 +0100

    Added the ln script

---
+++ ln.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PWDIR=`pwd`;
+
+ln -s ${PWDIR}/.gemrc ~/.gemrc
+ln -s ${PWDIR}/.gitconfig ~/.gitconfig
+ln -s ${PWDIR}/.gitignore ~/.gitignore
+ln -s ${PWDIR}/.irbrc ~/.irbrc
+ln -s ${PWDIR}/.profile ~/.profile
+ln -s ${PWDIR}/.stack ~/.stack
+ln -s ${PWDIR}/.tmux ~/.tmux
+ln -s ${PWDIR}/.tmux.conf ~/.tmux.conf
+ln -s ${PWDIR}/.vim ~/.vim
+ln -s ${PWDIR}/.vimrc ~/.vimrc
